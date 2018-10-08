04267eb72540ef95dd1e593d70efe0d2f91d2f6a
JuhanaKivela <kivelajuhana@gmail.com>
2018-10-08 21:15:34 +0200

    Sum beam testing

---
+++ Assets/Scripts/UfoBeamScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoBeamScript : MonoBehaviour {
+
+    public ConstantForce gravity;
+    private float maxBeamableWeight = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider colliderObject)
+    {
+        
+    }
+}
