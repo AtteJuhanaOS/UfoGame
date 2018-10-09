3b7cecece37cdef22f3c5c5130cbbb352e0c5699
JuhanaKivela <kivelajuhana@gmail.com>
2018-10-09 21:06:59 +0200

    Beam (almost) working now

--- Assets/Scripts/UfoBeamScript.cs
+++ Assets/Scripts/UfoBeamScript.cs
@@ -6,18 +6,36 @@ public class UfoBeamScript : MonoBehaviour {
 
     public ConstantForce gravity;
     private float maxBeamableWeight = 5;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+
 	}
 
     private void OnTriggerEnter(Collider colliderObject)
     {
-        
+        if(colliderObject.gameObject.tag=="BeamableObject")
+        {
+            ConstantForce forceToApply;
+            forceToApply = colliderObject.gameObject.AddComponent<ConstantForce>();
+            forceToApply.force = new Vector3(0, colliderObject.gameObject.GetComponent<BeamableObjectScript>().objectWeight*12, 0);
+            
+        }
     }
+
+    private void OnTriggerExit(Collider colliderObject)
+    {
+        if (colliderObject.gameObject.tag == "BeamableObject")
+        {
+            var constantForce = colliderObject.gameObject.GetComponent<ConstantForce>();
+            Destroy(constantForce);
+            Debug.Log("DESTROY: " + colliderObject);
+        }
+    } 
 }
